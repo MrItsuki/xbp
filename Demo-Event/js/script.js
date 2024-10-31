1c140db2e300fb7f2ff393f24fb07cba162a981c
MrItsuki <r202302249nq@jindai.jp>
2024-10-31 15:56:33 +0900

    20241031_2

--- Demo-Event/js/script.js
+++ Demo-Event/js/script.js
@@ -3,9 +3,9 @@ const nav = document.querySelector('.main-nav');
 
 btn.addEventListener('click', () => {
   nav.classList.toggle('open-menu');
-  if (btn.innerHTML === 'Menu') {
-    btn.innerHTML = 'Close';
+  if (btn.innerHTML === 'メニュー') {
+    btn.innerHTML = '閉じる';
   } else {
-    btn.innerHTML = 'Menu';
+    btn.innerHTML = 'メニュー';
   }
 });
