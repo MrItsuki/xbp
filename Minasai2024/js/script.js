bdf0642c7fb2a979bd9c6f1e767e2ef95c0ab3ea
MrItsuki <r202302249nq@jindai.jp>
2024-11-01 10:20:00 +0900

    20241101_3

--- Minasai2024/js/script.js
+++ Minasai2024/js/script.js
@@ -10,3 +10,17 @@ btn.addEventListener('click', () => {
   }
 });
 
+$(document).ready(function() {
+  var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+            }
+       });
+       pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+              return false;
+   });
+});
